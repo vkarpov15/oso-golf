334b9c542f57ac4ec4931c60800f9787022b3f8e
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2023-10-19 14:47:55 -0400

    refactor constraints

--- src/components.js
+++ src/components.js
@@ -1,8 +1,9 @@
 'use strict';
 
-// This file is auto-generated. Do not modify this file directly.
+  // This file is auto-generated. Do not modify this file directly.
 exports['app-component'] = require('./app-component/app-component.js');
 exports['async-button'] = require('./async-button/async-button.js');
+exports['constraints'] = require('./constraints/constraints.js');
 exports['leaderboard'] = require('./leaderboard/leaderboard.js');
 exports['level'] = require('./level/level.js');
 exports['splash-screen'] = require('./splash-screen/splash-screen.js');
---
+++ src/constraints/constraints.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const template = require('./constraints.html');
+
+module.exports = app => app.component('constraints', {
+  inject: ['state'],
+  template: template,
+  methods: {
+    displayImageForTestResult(index) {
+      if (!this.state.results[index]) {
+        return '/images/loader.gif';
+      }
+      return this.state.results[index].pass ? '/images/check-green.svg' : '/images/error-red.svg';
+    }
+  },
+  computed: {
+    testsInProgress() {
+      return this.state.constraints.length > 0 && this.state.constraints.length !== this.state.results.length;
+    },
+  }
+});
