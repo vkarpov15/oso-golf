33aff8d5c4ab3ff7b5e858e5fee640e11fc1b8a3
Valeri Karpov <val@karpov.io>
2023-11-07 12:35:00 -0500

    new footer

--- src/components.js
+++ src/components.js
@@ -1,10 +1,11 @@
 'use strict';
 
-// This file is auto-generated. Do not modify this file directly.
+  // This file is auto-generated. Do not modify this file directly.
 exports['add-role-fact'] = require('./add-role-fact/add-role-fact.js');
 exports['app-component'] = require('./app-component/app-component.js');
 exports['async-button'] = require('./async-button/async-button.js');
 exports['constraints'] = require('./constraints/constraints.js');
 exports['leaderboard'] = require('./leaderboard/leaderboard.js');
 exports['level'] = require('./level/level.js');
+exports['oso-footer'] = require('./oso-footer/oso-footer.js');
 exports['splash-screen'] = require('./splash-screen/splash-screen.js');
---
+++ src/oso-footer/oso-footer.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const template = require('./oso-footer.html');
+
+module.exports = app => app.component('oso-footer', {
+  template
+});
