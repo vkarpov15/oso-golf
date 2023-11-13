3a7e4cd31ab29367fa9bf8cdbc5475b56a2dffcb
Valeri Karpov <val@karpov.io>
2023-11-13 10:51:32 -0500

    avoid crashing on null player property

--- src/share/share.js
+++ src/share/share.js
@@ -7,6 +7,9 @@ module.exports = app => app.component('share', {
   inject: ['state'],
   computed: {
     twitterLink() {
+      if (!this.state.player) {
+        return '';
+      }
       const displayPar = this.state.player.par < 0
         ? '' + this.state.player.par
         : '+' + this.state.player.par;
@@ -21,6 +24,10 @@ module.exports = app => app.component('share', {
       return `http://twitter.com/share?text=${twitterShareText}&url=${shareUrl}`;
     },
     linkedInLink() {
+      if (!this.state.player) {
+        return '';
+      }
+
       const shareUrl = encodeURIComponent(
         'https://oso-golf.netlify.app/.netlify/functions/share?sessionId=' +
           encodeURIComponent(this.state.player.sessionId)
